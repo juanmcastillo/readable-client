6153657540b4ade12adfd7a777938623bf0c5fa2
Juan M. Castillo Lirio <juanmanuelcastillolirio@gmail.com>
2018-01-22 12:02:33 -0400

    created utility resource to communicate with back-end

---
+++ src/utils/ReadableApi.js
@@ -0,0 +1,44 @@
+const api = process.env.REACT_APP_READABLE_API_URL || 'http://localhost:3001';
+
+let token = localStorage.token;
+
+if (!token){
+    token = localStorage.token = Math.random().toString(36).substr(-8);
+}
+  
+const headers = {
+  'Accept': 'application/json',
+  'Authorization': token
+};
+
+/* Categories End Points */
+
+export const getAllCategories = () =>
+  fetch(`${api}/categories`, { method: 'GET', headers })
+    .then(res => res.json());
+
+export const getAllCategoryPosts = (category) =>
+  fetch(`${api}/${category}/posts`, { method: 'GET', headers })
+    .then(res => res.json());
+
+/* Posts End Points */
+
+export const getAllPosts = (category) =>
+    fetch(`${api}/posts`, { method: 'GET', headers })
+      .then(res => res.json());
+
+export const getPostById = (postId) =>
+    fetch(`${api}/posts/${postId}`, { method: 'GET', headers })
+      .then(res => res.json());
+
+export const createPost = (body) =>
+    fetch(`${api}/posts`, 
+          {
+            method: 'POST',
+            headers: {
+                ...headers,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(body)
+          })
+    .then(res => res.json());
