26ed726d36321ae282a6c2b1165b63822fd574b9
Juan M. Castillo Lirio <juanmanuelcastillolirio@gmail.com>
2018-01-24 02:21:12 -0400

    included thunk to the project.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import ReducerRegistry from './reducers/ReducerRegistry';
 import { BrowserRouter } from 'react-router-dom';
@@ -9,7 +10,10 @@ import registerServiceWorker from './registerServiceWorker';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import './styles/App.css';
 
-const store = createStore(ReducerRegistry, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const middlewares = compose(applyMiddleware(thunk), 
+                            window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+const store = createStore(ReducerRegistry,  middlewares);
 
 const app = <Provider store={store}>
                 <BrowserRouter>
